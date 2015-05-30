b11b21f87b376c04fc6353b912a2d5904b6955c7
Michael Bastos <bastosmichael@gmail.com>
2015-05-30 14:08:24 -0700

    Removing sub folders

--- config/application.rb
+++ config/application.rb
@@ -20,17 +20,15 @@ module RailsStripeMembershipSaas
 
     # don't generate RSpec tests for views and helpers
     config.generators do |g|
-      
+
       g.test_framework :rspec, fixture: true
       g.fixture_replacement :factory_girl, dir: 'spec/factories'
-      
-      
+
+
       g.view_specs false
       g.helper_specs false
     end
 
-    # Load Subfolder Models
-    config.autoload_paths += Dir[Rails.root.join('app', 'models', '{**}')]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
